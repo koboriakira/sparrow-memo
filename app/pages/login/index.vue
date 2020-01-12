5db9a048a7b3e1fb21c1e1a6d9e675096896276b
Akira Kobori <private.beats@gmail.com>
2020-01-13 01:24:35 +0900

    Add a Login page

---
+++ app/pages/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline">Login</v-card-title>
+        <v-card-text>
+          <v-btn>signIn</v-btn>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
